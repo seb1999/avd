a44538c7fee375b1bfb543a5561cb02c6485c5e4
seb1999 <sebastian.addams@outlook.com>
2022-05-25 19:25:01 +1000

    Update and rename downloadandinstallfslogix to downloadandinstallfslogix.ps1

--- downloadandinstallfslogix.ps1
+++ downloadandinstallfslogix.ps1
@@ -1,8 +1,8 @@
 write-host 'AIB Customization: Downloading FsLogix'
 New-Item -Path C:\\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
 $LocalPath = 'C:\\fslogix'
-$WVDflogixURL = 'https://raw.githubusercontent.com/DeanCefola/Azure-WVD/master/PowerShell/FSLogixSetup.ps1'
-$WVDFslogixInstaller = 'FSLogixSetup.ps1'
+$AVDflogixURL = 'https://raw.githubusercontent.com/DeanCefola/Azure-WVD/master/PowerShell/FSLogixSetup.ps1'
+$AVDFslogixInstaller = 'FSLogixSetup.ps1'
 $outputPath = $LocalPath + '\' + $WVDFslogixInstaller
 Invoke-WebRequest -Uri $WVDflogixURL -OutFile $outputPath
 set-Location $LocalPath
