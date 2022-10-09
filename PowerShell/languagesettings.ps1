b3732710fbe22d3f2766b115ef37f30190093090
seb1999 <sebastian.addams@outlook.com>
2022-10-09 21:03:39 +1100

    Update languagesettings.ps1

--- PowerShell/languagesettings.ps1
+++ PowerShell/languagesettings.ps1
@@ -1,4 +1,5 @@
 ﻿Set-WinSystemLocale -SystemLocale en-AU
+Set-WinUILanguageOverride -Language en-AU
 Set-TimeZone -Id "AUS Eastern Standard Time"
 Set-WinUserLanguageList -LanguageList "en-AU" -Force
 shutdown -r -f -t 00
