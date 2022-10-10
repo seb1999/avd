ab3bfb29dd875aff4efbd98d2cbf7c64e0d3e6af
seb1999 <sebastian.addams@outlook.com>
2022-10-10 21:32:57 +1100

    Create BuildAIBEnvironment.ps1

---
+++ PowerShell/BuildAIBEnvironment.ps1
@@ -0,0 +1,159 @@
+﻿Import-Module Az.Accounts
+Connect-AzAccount
+
+
+
+
+
+# Register for Azure Image Builder Feature
+#Register-AzProviderFeature -FeatureName VirtualMachineTemplatePreview -ProviderNamespace Microsoft.VirtualMachineImages
+
+#Get-AzProviderFeature -FeatureName -like VirtualMachine -ProviderNamespace Microsoft.VirtualMachineImages
+# wait until RegistrationState is set to 'Registered'
+
+# check you are registered for the providers, ensure RegistrationState is set to 'Registered'.
+#Get-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
+#Get-AzResourceProvider -ProviderNamespace Microsoft.Storage 
+#Get-AzResourceProvider -ProviderNamespace Microsoft.Compute
+#Get-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
+
+# If they do not saw registered, run the commented out code below.
+
+Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
+Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
+Register-AzResourceProvider -ProviderNamespace Microsoft.Compute
+Register-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
+
+
+
+
+
+# get existing context
+$currentAzContext = Get-AzContext
+
+# destination image resource group
+$imageResourceGroup="rg-avdimage"
+
+# location (see possible locations in main docs)
+$location="australiaEast"
+
+# your subscription, this will get your current subscription
+$subscriptionID=$currentAzContext.Subscription.Id
+
+# image template name
+$imageTemplateName="avdtemplate"
+
+# distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+$runOutputName="sigOutput"
+
+# create resource group
+New-AzResourceGroup -Name $imageResourceGroup -Location $location
+
+
+
+
+# setup role def names, these need to be unique
+$imageRoleDefName="Azure Image Builder Image Def"
+$idenityName="aibIdentity"
+
+## Add AZ PS modules to support AzUserAssignedIdentity and Az AIB
+'Az.ImageBuilder', 'Az.ManagedServiceIdentity' | ForEach-Object {Import-Module -Name $_}
+
+# create identity
+New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName -Location $location
+
+$idenityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).Id
+$idenityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).PrincipalId
+
+
+
+
+
+$aibRoleImageCreationUrl="https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
+$aibRoleImageCreationPath = "aibRoleImageCreation.json"
+
+# download config
+Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
+
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
+
+# create role definition
+New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
+
+# grant role definition to image builder service principal
+New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+
+### NOTE: If you see this error: 'New-AzRoleDefinition: Role definition limit exceeded. No more role definitions can be created.' See this article to resolve:
+### https://docs.microsoft.com/en-us/azure/role-based-access-control/troubleshooting
+
+
+
+
+
+$sigGalleryName= "AVDComputeLibrary"
+$imageDefName ="win11-avd"
+
+# create gallery
+New-AzGallery -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup  -Location $location
+
+
+
+
+
+# create gallery image definition
+$galleryImageDefinitionName = "win11-avd"
+$publisherName = "microsoftwindowsdesktop"
+$offerName = "office-365"
+$skuName = "win11-21h2-avd-m365"
+$IsAcceleratedNetworkSupported = @{Name='IsAcceleratedNetworkSupported';Value='True'}
+$features = @($IsAcceleratedNetworkSupported)
+New-AzGalleryImageDefinition -ResourceGroupName $imageResourceGroup -GalleryName $sigGalleryName -Name $imageDefName -Location $location -Publisher $publisherName -Offer $offerName -Sku $skuName -OsState "Generalized" -OsType "Windows" -HyperVGeneration "V2" -Feature $features
+
+
+
+
+# create image template
+$templateFilePath = "armTemplateWVD.json"
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2020-02-14" -imageTemplateName $imageTemplateName -svclocation $location
+
+
+
+
+
+# Optional - if you have any errors running the above, run:
+$getStatus=$(Get-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName)
+$getStatus.ProvisioningErrorCode 
+$getStatus.ProvisioningErrorMessage
+
+
+
+
+# start building the image
+Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName -NoWait
+
+
+#status polling
+start-sleep -seconds 20
+Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2020-02-14" -Action Run -Force
+
+#Get Status of the Image Build and Query
+$resourcetowatch = Get-AzResource -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName
+do {
+    $status = (Get-AzResource -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName).Properties.lastRunStatus
+    $status | Format-Table *
+    Start-Sleep -Seconds 720
+    } 
+    while ($status.runState -eq "Running")
+    #Until ($status.rubSubState -eq "Distributing")
+
+
+
+
+
+
+# Clean up
+$resTemplateId = Get-AzResource -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2019-05-01-preview"
+Remove-AzResource -ResourceId $resTemplateId.ResourceId -Force
+Remove-AzResourceGroup $imageResourceGroup -Force
